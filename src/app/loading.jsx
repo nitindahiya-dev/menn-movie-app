1b2ef21ccfd1b19668f5d85ace3df95d7fadfa9b
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-09 16:17:12 +0530

    add loader

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+
 const loading = () => {
   return (
     <div className='flex justify-center'>
-        <Image className='h-52' src="loader.svg" alt="loading.." />
+        <img className='h-52' src="loader.svg" alt="loading.." />
     </div>
   )
 }
